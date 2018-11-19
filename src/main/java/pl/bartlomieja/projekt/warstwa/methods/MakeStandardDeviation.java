74dba56404d2b8e2f8642d6fb96d5b5c8d5f950c
Bartłomiej <bartlomiejgluch@gmail.com>
2018-11-19 16:12:00 +0100

    add turbulence intensity and refactor GUI

--- src/main/java/pl/bartlomieja/projekt/warstwa/methods/MakeStandardDeviation.java
+++ src/main/java/pl/bartlomieja/projekt/warstwa/methods/MakeStandardDeviation.java
@@ -2,22 +2,91 @@ package pl.bartlomieja.projekt.warstwa.methods;
 
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import pl.bartlomieja.projekt.warstwa.IO.RowDataObject;
+
+import java.util.ArrayList;
 
 public class MakeStandardDeviation {
 
 
-    public static void main(String[] args) {
+    public void calculateStdDev(ArrayList<Object> allRowDataObject, ArrayList<StandardDeviationObject> standardDeviationObjects) {
 
 
-        DescriptiveStatistics stats = new DescriptiveStatistics();
-        stats.addValue(2);
 
-        stats.addValue(4);
-        stats.addValue(1);
-        stats.addValue(98);
+        for (int i = 0; i < allRowDataObject.size(); i++) {
 
-        stats.getStandardDeviation();
 
-        System.out.println(stats.getStandardDeviation());
+            Object anAllRowDataObject = allRowDataObject.get(i);
+
+            ArrayList<RowDataObject> dataArrayToAverage = (ArrayList<RowDataObject>) anAllRowDataObject;
+
+            DescriptiveStatistics stdDevObjectStatsUx1 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUy1 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUz1 = new DescriptiveStatistics();
+
+            DescriptiveStatistics stdDevObjectStatsUx2 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUy2 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUz2 = new DescriptiveStatistics();
+
+            DescriptiveStatistics stdDevObjectStatsUx3 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUy3 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUz3 = new DescriptiveStatistics();
+
+            DescriptiveStatistics stdDevObjectStatsUx4 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUy4 = new DescriptiveStatistics();
+            DescriptiveStatistics stdDevObjectStatsUz4 = new DescriptiveStatistics();
+
+
+            for (int j = 0; j < dataArrayToAverage.size(); j++) {
+
+
+                stdDevObjectStatsUx1.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUx1()));
+                stdDevObjectStatsUy1.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUy1()));
+                stdDevObjectStatsUz1.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUz1()));
+
+                stdDevObjectStatsUx2.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUx2()));
+                stdDevObjectStatsUy2.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUy2()));
+                stdDevObjectStatsUz2.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUz2()));
+
+                stdDevObjectStatsUx3.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUx3()));
+                stdDevObjectStatsUy3.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUy3()));
+                stdDevObjectStatsUz3.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUz3()));
+
+                stdDevObjectStatsUx4.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUx4()));
+                stdDevObjectStatsUy4.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUy4()));
+                stdDevObjectStatsUz4.addValue(Double.parseDouble(dataArrayToAverage.get(j).getUz4()));
+
+            }
+
+
+            StandardDeviationObject nextStandardDeviationObject = new StandardDeviationObject(
+                    stdDevObjectStatsUx1.getStandardDeviation(),
+                    stdDevObjectStatsUy1.getStandardDeviation(),
+                    stdDevObjectStatsUz1.getStandardDeviation(),
+                    stdDevObjectStatsUx2.getStandardDeviation(),
+                    stdDevObjectStatsUy2.getStandardDeviation(),
+                    stdDevObjectStatsUz2.getStandardDeviation(),
+                    stdDevObjectStatsUx3.getStandardDeviation(),
+                    stdDevObjectStatsUy3.getStandardDeviation(),
+                    stdDevObjectStatsUz3.getStandardDeviation(),
+                    stdDevObjectStatsUx4.getStandardDeviation(),
+
+                    stdDevObjectStatsUy4.getStandardDeviation(),
+                    stdDevObjectStatsUz4.getStandardDeviation(),
+                    dataArrayToAverage.size(), i);
+
+
+            standardDeviationObjects.add(nextStandardDeviationObject);
+
+
+
+
+        }
+
+
     }
+
+
 }
+
+
